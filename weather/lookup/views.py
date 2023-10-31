7826e344d99919aac58db4002652e01e35e12e6a
prabhat shukla <prabhat.shukla30990@gmail.com>
2023-10-31 22:45:18 +0000

    converted to actual forms

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -4,16 +4,29 @@ import json
 
 # Create your views here.
 def home(request):
-    urlss = "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=10004&distance=25&API_KEY=E8603FBE-DBC5-442D-BD85-7869C7B507B7"
-    try:
-        api_request = requests.get(urlss)
-        print("api_request", api_request.content)
 
-        api_res = json.loads(api_request.content)
-        print("api_res",api_res)
-        return render(request,'home.html',{"api":api_res})
-    except: 
-        return render(request,'error.html',{})
+
+    if request.method == "POST":
+        zipcode = request.POST['zipcode']
+        urlss = f"https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode={zipcode}&distance=25&API_KEY=E8603FBE-DBC5-442D-BD85-7869C7B507B7"
+        try:
+            api_request = requests.get(urlss)
+            api_res = json.loads(api_request.content)
+            api_res[0]["zipcode"]= zipcode
+            print(api_res)
+            return render(request,'home.html',{"api":api_res})
+        except: 
+            return render(request,'error.html',{})
+        # return render(request, 'home.html', {'zipcode':zipcode})
+
+    else:
+        urlss = "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=10004&distance=25&API_KEY=E8603FBE-DBC5-442D-BD85-7869C7B507B7"
+        try:
+            api_request = requests.get(urlss)
+            api_res = json.loads(api_request.content)
+            return render(request,'home.html',{"api":api_res})
+        except: 
+            return render(request,'error.html',{})
  
 
 
